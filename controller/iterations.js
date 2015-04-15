8b154ba8426ca71abb669ac73d0cf07408127307
Hinson <hinson0@qq.com>
2015-04-15 17:07:12 +0800

    * 使用sequelize重构

--- controller/iterations.js
+++ controller/iterations.js
@@ -2,90 +2,118 @@ var express = require('express');
 var router = express.Router();
 var moment = require('moment');
 
-var IterationModel = require('../model/iteration');
-var VersionModel = require('../model/version');
-var IterationService = require('../service/iteration');
+var VersionModel2 = require('../model/version2');
+var IterationModel2 = require('../model/iteration2');
+var IterationService2 = require('../service/iteration2');
 
-router.use('/:id', function(req, res, next) {
-    IterationModel.getById(req.params.id, function(interationInfo) {
-        if (!interationInfo.id) {
-            res.status(404);
-            res.json({'msg': '迭代不存在'});
-        } else {
-            req.interationInfo = interationInfo;
-            next();
-        }
+router.use('/:id', function (req, res, next) {
+  IterationModel2
+    .find(req.params.id)
+    .then(function(iteration) {
+      if (iteration === null) {
+        res.status(404);
+        res.json({msg: '迭代不存在'});
+      } else {
+        req.iteration = iteration;
+        next();
+      }
     });
 });
 
 // 列表
-router.get('/', function(req, res, next) {
-    var pagination = {
-        offset: req.query.offset || 0,
-        size: req.query.size || 10
-    };
-    var condition = {
-        versionId: req.query.version_id
-    };
-    IterationService.getListWithProject(condition, pagination, req.param('_include_total', true), function(results) {
-        res.json(results);
-    })
+router.get('/', function (req, res, next) {
+    IterationService2.formatList(req.query, function(error, result) {
+      res.json(result);
+    });
 });
 
 // 新增
 router.post('/', checkVersionId, checkUniqVN);
-router.post('/', function(req, res) {
-    var interation = new IterationModel();
-    var props = req.body;
-    props.create_time = moment().unix();
-    interation.save(props, function(interationId) {
-        res.json({id: interationId});
+router.post('/', function (req, res) {
+  IterationModel2
+    .build(req.body)
+    .save()
+    .then(function(iteration) {
+      res.json({id: iteration.id});
+    })
+    .catch(function(error) {
+      res.status(404);
+      res.json(error.errors);
     });
 });
 
 // 编辑
 router.put('/:id', checkVersionId);
-router.put('/:id', function(req, res, next) {
-    if ((req.interationInfo.name !== req.body.name) || (req.interationInfo.version_id !== req.body.version_id)) {
-        checkUniqVN(req, res, next);
-    } else {
-        next();
-    }
+router.put('/:id', function (req, res, next) {
+  if ((req.iteration.name !== req.body.name) || (req.iteration.version_id !== req.body.version_id)) {
+    checkUniqVN(req, res, next);
+  } else {
+    next();
+  }
 });
-router.put('/:id', function(req, res) {
-    var interation = new IterationModel(false);
-    var props = req.body;
-    props.id = req.params.id;
-    interation.save(props, function(interationId) {
-        res.json({id: interationId});
+router.put('/:id', function (req, res) {
+  console.log(12);
+  console.log(req.iteration);
+  var iteration = req.iteration;
+  iteration
+    .updateAttributes(req.body)
+    .then(function() {
+      res.json({id: iteration.id});
+    })
+    .catch(function(error) {
+      res.status(404);
+      res.json(error.errors);
     });
 });
 
+// 关闭
+router.put('/:id/closed', function (req, res) {
+  var iteration = req.iteration;
+  iteration.status = IterationModel2.statusClosed;
+  iteration.save().then(function () {
+    res.json({msg: '关闭成功'});
+  });
+});
+
 // 删除
-router.delete('/:id', function(req, res) {
-    IterationModel.deleteById(req.params.id, function() {
-        res.json({'msg': '删除成功'});
+router.delete('/:id', function (req, res) {
+  var iteration = req.iteration;
+  iteration.status = IterationModel2.statusOffline;
+  iteration
+    .save()
+    .then(function() {
+      res.json({msg: '删除成功'});
     });
 });
 
 function checkVersionId(req, res, next) { // 检查版本ID是否存在
-    VersionModel.getById(req.body.version_id, function(versionInfo) {
-        if (!versionInfo.id) {
-            res.status(404);
-            res.json({'msg': '版本号不存在'});
-        } else {
-            next();
-        }
+  VersionModel2
+    .find(req.body.version_id)
+    .then(function(version) {
+      if (version) {
+        next();
+      } else {
+        res.status(404);
+        res.json({msg: '版本号不存在'});
+      }
     });
 }
 function checkUniqVN(req, res, next) { // 校验迭代名称是否存在
-    IterationModel.getByUniqVN(req.body.version_id, req.body.name, function(interationInfo) {
-        if (interationInfo.id) {
-            res.status(404);
-            res.json({'msg': '迭代名称存在'});
-        } else {
-            next();
-        }
+  IterationModel2
+    .find({
+      where: {
+        version_id: req.body.version_id,
+        name: req.body.name,
+      }
+    })
+    .then(function(iteration) {
+      console.log(iteration);
+      if (iteration) {
+        res.status(404);
+        res.json({msg: '迭代名称存在'});
+      } else {
+        next();
+      }
     });
 }
 
