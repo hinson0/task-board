00f9db59a1b9ff1cc454a2e9623fd735f47d18a0
Hinson <hinson0@qq.com>
2015-04-15 17:03:16 +0800

    * 测试代码

--- controller/index.js
+++ controller/index.js
@@ -2,13 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 var PostModel = require('../model/post');
+var IterationModel = require('../model/iteration2');
+var VersionModel = require('../model/version2');
+var ProjectModel = require('../model/project2');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  PostModel.find(1).then(function(post) {
-    console.log(post);
-    res.json(post);
-  });
+  IterationModel
+    .find({
+      where: {
+        id: 21
+      },
+      include: [
+        {model: VersionModel, include: [ProjectModel]},
+      ]
+    })
+    .then(function(iteration) {
+      res.json(iteration);
+    });
 });
 
 module.exports = router;
