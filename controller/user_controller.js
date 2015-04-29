75fc0698b9567e0d8aaab5dd6b4e8998022bb826
Hinson <hinson0@qq.com>
2015-04-29 09:09:00 +0800

    * 删除冗余文件 * 重构部分代码

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -4,30 +4,30 @@ var UserService = require('../service/user_service');
 
 // 登陆
 router.post('/login', function (req, res) {
-    // 请求xxjia.cn的登陆地址
-    UserService.loginFromXxjia(req.body, function (err, loginInfo) {
-        // 异常
-        if (err) {
-            res.status(404);
-            res.json(err);
-            return;
-        }
+  // 请求xxjia.cn的登陆地址
+  UserService.loginFromXxjia(req.body, function (err, loginInfo) {
+    // 异常
+    if (err) {
+      res.status(404);
+      res.json(err);
+      return;
+    }
 
-        // 是否正常请求
-        if (!loginInfo.uid) {
-            res.status(404);
-            res.json({msg: loginInfo.error.msg});
-            return;
-        }
+    // 是否正常请求
+    if (!loginInfo.uid) {
+      res.status(404);
+      res.json({msg: loginInfo.error.msg});
+      return;
+    }
 
-        UserService.saveWhenLoginFromXxjia(loginInfo, req.body.account, function(user) {
-            res.json({id: user.id});
-        });
+    UserService.saveWhenLoginFromXxjia(loginInfo, req.body.account, function (user) {
+      res.json({id: user.id});
     });
+  });
 });
 
 // 登出
-router.post('/logout', function() {
+router.post('/logout', function () {
 
 });
 
