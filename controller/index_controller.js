11a1522d37f6e81a91b2ebfcdf97d6dc1814c255
Hinson <hinson0@qq.com>
2015-07-08 20:15:00 +0800

    * 进一步简化代码

--- controller/index_controller.js
+++ controller/index_controller.js
@@ -1,17 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var async = require('async');
-var moment = require('moment');
-var fs = require('fs');
-var _ = require('underscore');
-
-var Sequelize = require('sequelize');
-var IterationModel = require('../model/iteration_model');
-var VersionModel = require('../model/version_model');
-var ProjectModel = require('../model/project_model');
-var TaskModel = require('../model/task_model');
-
-var Logger = require('../library/logger');
 
 /* GET home page. */
 router.get('/', function (req, res) {
